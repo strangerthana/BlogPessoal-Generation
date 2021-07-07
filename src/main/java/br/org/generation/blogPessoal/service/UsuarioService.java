8e6d2ce865400f9240c7995487d5a5e18b4e9ba8
Thana <thanailde.oliveira@outlook.com>
2021-07-06 22:13:12 -0300

    Atualizando o blogPessoal, implementando a security.

---
+++ src/main/java/br/org/generation/blogPessoal/service/UsuarioService.java
@@ -0,0 +1,59 @@
+package br.org.generation.blogPessoal.service;
+
+import java.nio.charset.Charset;
+import java.util.Optional;
+
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import br.org.generation.blogPessoal.model.UsuarioLogin;
+import br.org.generation.blogPessoal.model.Usuario;
+import br.org.generation.blogPessoal.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	public Usuario cadastrarUsuario(Usuario usuario) {
+		
+		if(usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent())
+			
+			return null;
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		
+		String senhaEncoder = encoder.encode(usuario.getSenha());
+		
+		usuario.setSenha(senhaEncoder);
+		
+		return usuarioRepository.save(usuario);
+		
+		
+	}
+	
+	public Optional<UsuarioLogin> logarUsuario(Optional<UsuarioLogin> usuarioLogin){
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		Optional<Usuario> usuario = usuarioRepository.findByUsuario(usuarioLogin.get().getUsuario());
+		
+		if(usuario.isPresent()) {
+			if(encoder.matches(usuarioLogin.get().getSenha(), usuario.get().getSenha())) {
+				
+				String auth = usuarioLogin.get().getUsuario() + ":" + usuarioLogin.get().getSenha();
+				byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+				String authHeader = "Basic " + new String(encodedAuth);
+				
+				usuarioLogin.get().setToken(authHeader);				
+				usuarioLogin.get().setNome(usuario.get().getNome());
+				usuarioLogin.get().setSenha(usuario.get().getSenha());
+				
+				return usuarioLogin;
+
+			}
+		}
+		return null;
+	}
+
+}
